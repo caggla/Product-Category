8396a60199df9d718cef00b6733a4cb2504337dc
caglacorakk <corakcagla021@gmail.com>
2022-09-09 01:11:48 +0300

    onClick fonk. cagirmak

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -12,7 +12,11 @@ export default class CategoryList extends Component {
   }*/ // aşagıdaki şekılde kullanabiliriz yeni versiyonda :D
   state = {
     categories: [{ categoryId: 1, categoryName: "Beverages" },
-    { categoryId: 2, categoryName: "Condiments" }]
+    { categoryId: 2, categoryName: "Condiments" }],
+    currentCategory: ""
+  }
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName })
   }
   render() {
     return (
@@ -23,10 +27,12 @@ export default class CategoryList extends Component {
         <ListGroup>
           {
             this.state.categories.map(category => ( //map hepsi icin donuyor kac veri varsa :)
-              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem> //key her bir elemanın id si gibi düşün :D --CATEGORY. DEDİK --
-            ))
+              //   <ListGroupItem onClick={() => this.setState({ currentCategory: category.categoryName })} //--->bu sekıldede yazılır su sekılde de ====
+              <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem> //key her bir elemanın id si gibi düşün :D --CATEGORY. DEDİK --
+            )) //burada ()=> this.setState önemli bu lambda func. olacak.
           }
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     )
   }
