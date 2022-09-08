37ab1e66347eb6deba10ebf2253c1a18dbe428f6
caglacorakk <corakcagla021@gmail.com>
2022-09-09 00:33:08 +0300

    Props

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,10 +3,14 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 
 
 export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+    state: { }
+  }
   render() {
     return (
       <div>
-        <h3>Category List</h3>
+        <h3>{this.props.title}</h3>
 
         <ListGroup>
           <ListGroupItem>Cras justo odio</ListGroupItem>
