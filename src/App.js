ba7e28b07300216896e8165c04fdcf65ad0ddebc
caglacorakk <corakcagla021@gmail.com>
2022-09-09 01:40:39 +0300

    Send data between components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+import React, { Component } from 'react'
 import Navi from './Navi.js';
 import CategoryList from './CategoryList.js';
 import ProductList from './ProductList.js';
 import './App.css';
 import { Container, Row, Col } from 'reactstrap';
 
-function App() {
-  // let titleProduct = "Product List" //Bunun yerine aşağı satır: 
-  let productInfo = { title: "Product List" }  //Bunun yerine aşağı satır: 
-  // let titleCategory = "Category List"  //Bunun yerine aşağı satır: 
-  //Nedeni de obje old gu ıcın title a ek olarak başka prop larda ekleyebiliriz :)
-  let categoryInfo = { title: "Category List" }
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  state = { currentCategory: "" } //ve asagıda bunu cagıracagız. boylece CategoryList.js deki this.state.currentCategory i this.props.currentCategory olarak degıstırecegız . :D
+
+  //ve categorydeki setState kısmını buraya almam gerekiyır.-->
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName })
+  }
+
+  render() {
+    // let titleProduct = "Product List" //Bunun yerine aşağı satır: 
+    let productInfo = { title: "Product List" }  //Bunun yerine aşağı satır: 
+    // let titleCategory = "Category List"  //Bunun yerine aşağı satır: 
+    //Nedeni de obje old gu ıcın title a ek olarak başka prop larda ekleyebiliriz :)
+    let categoryInfo = { title: "Category List" }
+    //aşağıda change category kısmını böyle yaparız. ve CategoryList componentinde bunu props olarak cagırcaz :D
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
+//burada onceden defult olarak function App idi. fakat ben componentler arası veri tasıyacagım ıcın App i class yaptım. ve categorydeki setState kısmını buraya almam gerekiyır.
+//ARTIK currentCategory={this.state.currentCategory} Yİ PRODUCT LİSTE DE TASITABİLİRİZ BÖYLECE CATEGORY SECILDIGINDE PRODUCT DEGISECEK :d
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -13,11 +13,9 @@ export default class CategoryList extends Component {
   state = {
     categories: [{ categoryId: 1, categoryName: "Beverages" },
     { categoryId: 2, categoryName: "Condiments" }],
-    currentCategory: ""
+    //   currentCategory: "" var idi burada ama artık props yaptıgımız içib bunu silmeliyiz . :D ve bunu state olarak App.js e alacaız. AMAC KATEGORY İSMİNE TIKLANDIGINDA PRODUCTIN DA O KATEGORY E GORE DEGISMESI . aPP PARENT CATEGORY OLD GU ICIN BUNLARI YAPIYORUZ YANİ SADECE TEL YONLU YAPABİLECEĞİMİZ İCİN APP TEN CATEGORY LİSTE VERİ GONDERIYORUZ.
   }
-  changeCategory = (category) => {
-    this.setState({ currentCategory: category.categoryName })
-  }
+
   render() {
     return (
       <div>
@@ -28,11 +26,11 @@ export default class CategoryList extends Component {
           {
             this.state.categories.map(category => ( //map hepsi icin donuyor kac veri varsa :)
               //   <ListGroupItem onClick={() => this.setState({ currentCategory: category.categoryName })} //--->bu sekıldede yazılır su sekılde de ====
-              <ListGroupItem onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem> //key her bir elemanın id si gibi düşün :D --CATEGORY. DEDİK --
+              <ListGroupItem onClick={() => this.props.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem> //key her bir elemanın id si gibi düşün :D --CATEGORY. DEDİK --
             )) //burada ()=> this.setState önemli bu lambda func. olacak.
           }
         </ListGroup>
-        <h4>{this.state.currentCategory}</h4>
+        <h4>{this.props.currentCategory}</h4>
       </div>
     )
   }
