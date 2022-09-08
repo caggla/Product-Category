cecb62088a705d1a5c278d8179c012800f81f99e
caglacorakk <corakcagla021@gmail.com>
2022-09-09 00:59:39 +0300

    State

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,21 +3,29 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 
 
 export default class CategoryList extends Component {
-  /* constructor(props) {
-     super(props);
-     state: { }
-   } */ //ZORUNLU DEĞİL YENİ VERSİYONLARDA :)
+  /*constructor(props) {
+    super(props);
+    this.state = {
+      categories: [{ categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" }]
+    }
+  }*/ // aşagıdaki şekılde kullanabiliriz yeni versiyonda :D
+  state = {
+    categories: [{ categoryId: 1, categoryName: "Beverages" },
+    { categoryId: 2, categoryName: "Condiments" }]
+  }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
 
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
+          {
+            this.state.categories.map(category => ( //map hepsi icin donuyor kac veri varsa :)
+              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem> //key her bir elemanın id si gibi düşün :D --CATEGORY. DEDİK --
+            ))
+          }
         </ListGroup>
       </div>
     )
